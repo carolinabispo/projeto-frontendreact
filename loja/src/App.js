cef389d55a23c8cbf6f915165813d3c038c5a240
carolinabispo <carolinabispos07@gmail.com>
2023-06-03 10:10:52 -0300

    [configuração]criei os compoenentes principais do projeto

--- loja/src/App.js
+++ loja/src/App.js
@@ -1,9 +1,19 @@
-
+import Filter from "./components/Filters/Filter";
+import Home from "./components/ProductList/Home/Home";
+import ProductCard from "./components/ProductList/ProductCard/ProductCard";
+import Cart from "./components/ShoppingCart/Cart/Cart";
+import Item from "./components/ShoppingCart/item/Item";
 
 function App() {
+  
   return (
     <div>
-        <h1>carol</h1>
+        <Home />
+        <Item />
+        <ProductCard />
+        <Filter />
+        <Cart />
+        
     </div>
   );
 }
